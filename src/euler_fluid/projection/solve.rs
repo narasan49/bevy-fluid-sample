1f27fefa5dda5a06f93a5dc3eaf63b38e6889331
narasan49 <narasan@hotmail.co.jp>
2024-08-20 21:04:19 +0900

    pressure material

--- src/euler_fluid/projection/solve.rs
+++ src/euler_fluid/projection/solve.rs
@@ -1,50 +1,36 @@
 use std::borrow::Cow;
 
 use bevy::{
-    asset::{AssetServer, Handle},
+    asset::AssetServer,
     prelude::*,
     render::{
-        extract_resource::ExtractResource,
-        render_asset::RenderAssets,
         render_resource::{
-            binding_types::uniform_buffer, AsBindGroup, BindGroup, BindGroupLayout,
-            BindGroupLayoutEntries, CachedComputePipelineId, ComputePipelineDescriptor,
-            PipelineCache, ShaderStages,
+            binding_types::uniform_buffer, AsBindGroup, BindGroupLayoutEntries,
+            CachedComputePipelineId, ComputePipelineDescriptor, PipelineCache, ShaderStages,
         },
         renderer::RenderDevice,
-        texture::{FallbackImage, GpuImage, Image},
     },
 };
 
-use crate::euler_fluid::{grid_label::GridLabelMaterial, uniform::SimulationUniform};
-
-#[derive(Resource, ExtractResource, AsBindGroup, Clone)]
-pub struct SolvePressureMaterial {
-    #[storage_texture(0, image_format = R32Float, access = ReadWrite)]
-    pub u_in: Handle<Image>,
-    #[storage_texture(1, image_format = R32Float, access = ReadWrite)]
-    pub v_in: Handle<Image>,
-    #[storage_texture(2, image_format = R32Float, access = ReadWrite)]
-    pub u_out: Handle<Image>,
-    #[storage_texture(3, image_format = R32Float, access = ReadWrite)]
-    pub v_out: Handle<Image>,
-    #[storage_texture(4, image_format = R32Float, access = ReadWrite)]
-    pub p: Handle<Image>,
-}
-
-#[derive(Resource)]
-pub struct SolvePressureBindGroup(pub BindGroup);
+use crate::euler_fluid::{
+    grid_label::GridLabelMaterial,
+    materials::{pressure::PressureMaterial, staggered_velocity::{IntermediateVelocityBindGroupLayout, VelocityBindGroupLayout}},
+    uniform::SimulationUniform,
+};
 
 #[derive(Resource)]
 pub struct SolvePressurePipeline {
-    bind_group_layout: BindGroupLayout,
     pub pipeline: CachedComputePipelineId,
 }
 
 impl FromWorld for SolvePressurePipeline {
     fn from_world(world: &mut bevy::prelude::World) -> Self {
         let render_device = world.resource::<RenderDevice>();
-        let bind_group_layout = SolvePressureMaterial::bind_group_layout(render_device);
+        let velocity_bind_group_layout = &world.resource::<VelocityBindGroupLayout>().0;
+        let intermediate_velocity_bind_group_layout =
+            &world.resource::<IntermediateVelocityBindGroupLayout>().0;
+        let pressure_bind_group_layout = PressureMaterial::bind_group_layout(render_device);
+
         let grid_label_bind_group_layout = GridLabelMaterial::bind_group_layout(render_device);
         let shader = world
             .resource::<AssetServer>()
@@ -62,7 +48,9 @@ impl FromWorld for SolvePressurePipeline {
         let pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
             label: Some(Cow::from("Solve Pressure")),
             layout: vec![
-                bind_group_layout.clone(),
+                velocity_bind_group_layout.clone(),
+                intermediate_velocity_bind_group_layout.clone(),
+                pressure_bind_group_layout.clone(),
                 uniform_bind_group_layout.clone(),
                 grid_label_bind_group_layout.clone(),
             ],
@@ -72,30 +60,6 @@ impl FromWorld for SolvePressurePipeline {
             entry_point: Cow::from("solve_pressure"),
         });
 
-        Self {
-            bind_group_layout,
-            pipeline,
-        }
+        Self { pipeline }
     }
-}
-
-pub fn prepare_bind_group(
-    mut commands: Commands,
-    render_device: Res<RenderDevice>,
-    gpu_images: Res<RenderAssets<GpuImage>>,
-    pipeline: Res<SolvePressurePipeline>,
-    material: Res<SolvePressureMaterial>,
-    fallback_image: Res<FallbackImage>,
-) {
-    let bind_group = material
-        .as_bind_group(
-            &pipeline.bind_group_layout,
-            &render_device,
-            &gpu_images,
-            &fallback_image,
-        )
-        .unwrap()
-        .bind_group;
-
-    commands.insert_resource(SolvePressureBindGroup(bind_group));
 }
