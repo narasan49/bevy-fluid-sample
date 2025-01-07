a651b40278f92556ac3605230b40a8ecda8e8c30
narasan49 <narasan49@users.noreply.github.com>
2025-01-07 23:35:54 +0000

    deploy: 375b934bbd938f7da182caa62350500ed3bfae03

--- out/interaction.js
+++ out/interaction.js
@@ -2745,7 +2745,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('bevy-fluid-sample_bg.wasm', import.meta.url);
+        input = new URL('interaction_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
